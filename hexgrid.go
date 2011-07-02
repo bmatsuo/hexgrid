65b29b4b23d6bd6fe4952a1d0922e155402b57af
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-07-02 15:38:50 -0700

    Reorder godoc again.

--- hexgrid.go
+++ hexgrid.go
@@ -12,4 +12,16 @@
 //  And is navigatable as a graph no matter whether a game uses tile
 //  connections, vertex connections, or both.
 //  
+//  The basic idea behind connections, as far as the API is concerned, is
+//  that of two core concepts in graph theory *incidence* and *adjacency*.
+//  Adjacency is a binary relation on objects of the same type. While,
+//  incidence is a binary relation on objects of strictly different type.
+//  
+//  For example, tiles are adjacent other tiles with which they share one
+//  edge. An object shared by two adjacent objects is incident with both
+//  those objects seperately. So, continuing the example, the shared edge
+//  between two adjacent tiles is incident with each tile. Similarly,
+//  the endpoints of that edge are shared between the adjacent tiles,
+//  so the end points (vertices) of the edge are also incident with both
+//  tiles.
 package hexgrid
